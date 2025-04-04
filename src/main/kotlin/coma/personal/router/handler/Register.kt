da096e93beb6d195f69fe2e1d7b9ea41b7f4ac2e
divasatanica <jayhebes@vip.qq.com>
2025-04-05 02:43:10 +0800

    Reuse existing connector while multiple registration happens

--- src/main/kotlin/coma/personal/router/handler/Register.kt
+++ src/main/kotlin/coma/personal/router/handler/Register.kt
@@ -7,23 +7,49 @@ import io.muserver.MuResponse
 import io.muserver.RouteHandler
 import org.jetbrains.exposed.sql.insertAndGetId
 import com.google.gson.Gson
+import jakarta.ws.rs.core.Response
 import kotlinx.datetime.Instant
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.sql.andWhere
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.util.UUID
 
 data class RegistrationPayload(val appName: String, val address: String)
 
 class RegisterHandler(
     private val registrationLoader: RegistrationLoader
+
 ): RouteHandler {
+    private val logger = org.slf4j.LoggerFactory.getLogger(javaClass)
     override fun handle(request: MuRequest, response: MuResponse, paramMap: MutableMap<String, String>?) {
         try {
             val body = request.readBodyAsString()
             val result = Gson().fromJson(body, RegistrationPayload::class.java)
+            val resultAppName = result.appName
+            val resultAddress = result.address
+            val fiveMinutesAgo = Instant.fromEpochMilliseconds(System.currentTimeMillis() - 300 * 1000)
+
             transaction {
-                val id = Connectors.insertAndGetId {
-                    it[appName] = result.appName
-                    it[address] = result.address
-                    it[lastActive] = Instant.fromEpochMilliseconds(System.currentTimeMillis())
+                // Found existing connector registered within 5 minutes (MAX heartbeat interval), reuse it
+                val existingList = Connectors.select(Connectors.fields)
+                    .where {
+                        Connectors.appName eq resultAppName
+                    }.andWhere {
+                        Connectors.address eq resultAddress
+                    }.andWhere {
+                        Connectors.lastActive greaterEq fiveMinutesAgo
+                    }
+                    .limit(1)
+                var id: EntityID<UUID>
+                if (existingList.count() > 0) {
+                    id = existingList.first()[Connectors.id]
+                    logger.info("Found existing connector with id: $id, reuse this connector")
+                } else {
+                    id = Connectors.insertAndGetId {
+                        it[appName] = result.appName
+                        it[address] = result.address
+                        it[lastActive] = Instant.fromEpochMilliseconds(System.currentTimeMillis())
+                    }
                 }
 
                 registrationLoader.refresh()
@@ -36,15 +62,11 @@ class RegisterHandler(
                     val code = 0
                 }
 
-
                 response.write(Gson().toJson(responseResult))
             }
         } catch (e: Exception) {
-            val responseResult = object {
-                val message = e.message
-                val code = -1
-            }
-            response.write(Gson().toJson(responseResult))
+            response.status(Response.Status.INTERNAL_SERVER_ERROR.statusCode)
+            response.write(e.message)
         }
     }
 }
